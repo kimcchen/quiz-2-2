dbfba48fce6c3fa17e6b46b2e80b4c7b2546e8f9
Kimberly Chen <kimberlykchen@gmail.com>
2023-03-23 18:57:53 +0000

    finished

--- printvars.c
+++ printvars.c
@@ -4,10 +4,26 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+
 char *custom_env[] = {"USER=Pikachu", "PATH=/tmp", NULL};
 
 int main(void)
 {
-    // TODO
+    int status;
+    printf("Running parent process PID: %d \n", getppid());
+    printf("\n");
+    pid_t pid = fork();
+    if(pid == 0) {
+        printf("Running child process PID: %d \n", getpid());
+        execle("./echoall", "echoall", "Bandicoot", "Pacman", (char*)NULL, custom_env);
+    } else {
+        waitpid(pid, &status, 0);
+        pid_t pid1 = fork();
+        if(pid1 == 0) {
+            printf("Running child process PID: %d \n", getpid());
+            const char *file = "echoall";
+            execlp(file, "echoall", "Spyro", (char*)NULL);
+        }
+    }
     exit(0);
 }
